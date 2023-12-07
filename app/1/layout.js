dc33e4de6e34075d49cc5cc591e7bccb41f44bab
Franco Ortega <francopdx@gmail.com>
2023-12-06 17:09:32 -0800

    move Nav to layout to make it global

--- app/1/layout.js
+++ app/1/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google';
+import Nav from './components/nav/Nav';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
--- app/1/page.js
+++ app/1/page.js
@@ -1,10 +1,8 @@
-import Nav from './components/nav/Nav';
 import styles from './page.module.css';
 
 export default function test1() {
   return (
     <div className={styles.test1}>
-      <Nav />
       This will be the first Portfolio test site. It is located in directory 1.
     </div>
   );
