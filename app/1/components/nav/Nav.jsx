ddcee65a28872a39109596a6d7d5add5cb76545e
Franco Ortega <francopdx@gmail.com>
2023-12-06 17:07:45 -0800

    create Nav component with filler links

---
+++ app/1/components/nav/Nav.jsx
@@ -0,0 +1,14 @@
+export default function Nav() {
+  return (
+    <nav>
+      Nav Bar
+      <ul>
+        <li>Link 1</li>
+        <li>Link 2</li>
+        <li>Link 3</li>
+        <li>Link 4</li>
+        <li>Link 5</li>
+      </ul>
+    </nav>
+  );
+}
