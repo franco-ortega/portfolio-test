4bf4009b52129111451bdbdb114e460050b5a979
Franco Ortega <francopdx@gmail.com>
2023-12-06 17:05:31 -0800

    update content

--- app/1/page.js
+++ app/1/page.js
@@ -1,5 +1,9 @@
 import styles from './page.module.css';
 
 export default function test1() {
-  return <div className={styles.test1}>Test 1</div>;
+  return (
+    <div className={styles.test1}>
+      This will be the first Portfolio test site. It is located in directory 1.
+    </div>
+  );
 }
