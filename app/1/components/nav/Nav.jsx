5dbaedddb4af346eb0b00678d2758c3811fc21b5
Franco Ortega <francopdx@gmail.com>
2023-12-07 17:22:11 -0800

    create Home link and Profile link in Nav

--- app/1/components/nav/Nav.jsx
+++ app/1/components/nav/Nav.jsx
@@ -1,10 +1,16 @@
+import Link from 'next/link';
 import styles from './Nav.module.css';
 
 export default function Nav() {
   return (
     <nav className={styles.Nav}>
       <ul>
-        <li>Link 1</li>
+        <li>
+          <Link href={'/1'}>Home</Link>
+        </li>
+        <li>
+          <Link href={'/1/profile'}>Profile</Link>
+        </li>
         <li>Link 2</li>
         <li>Link 3</li>
         <li>Link 4</li>
