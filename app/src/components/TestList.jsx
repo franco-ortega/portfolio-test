152e2a6c45494da984a28285cff38f97e3dd24d5
Franco Ortega <francopdx@gmail.com>
2023-12-11 12:59:27 -0800

    place TestList in div to nest it separately from main page content

--- app/src/components/TestList.jsx
+++ app/src/components/TestList.jsx
@@ -2,14 +2,13 @@ import Link from 'next/link';
 
 export default function TestList() {
   return (
-    <>
+    <div>
       <h2>Test List</h2>
       <ul>
         <li>
           <Link href={'/1'}>Portfolio Test 1</Link>
         </li>
       </ul>
-      ;
-    </>
+    </div>
   );
 }
